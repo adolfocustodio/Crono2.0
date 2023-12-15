fa3537369cdb1b59a92bf42592ef48df3b357556
adolfocustodio <adolfocustodio14@gmail.com>
2023-12-14 23:05:31 -0300

    sistema crud de categorias

--- core/views.py
+++ core/views.py
@@ -1,5 +1,29 @@
 from django.views import generic
+from .models import Categoria
+from .forms import CategoriaForm
+from django.urls import reverse_lazy
 
 
 class HomeView(generic.TemplateView):
-    template_name = "index.html"
+    template_name = "core/index.html"
+
+
+class CategoriaCreateView(generic.CreateView):
+    model = Categoria
+    form_class = CategoriaForm
+    success_url = reverse_lazy("core:categoria_listar")
+
+
+class CategoriaListView(generic.ListView):
+    model = Categoria
+
+
+class CategoriaUpdateView(generic.UpdateView):
+    model = Categoria
+    form_class = CategoriaForm
+    success_url = reverse_lazy("core:categorias_listar")
+
+
+class CategoriaDeleteView(generic.DeleteView):
+    model = Categoria
+    success_url = reverse_lazy("core:categoria_listar")
